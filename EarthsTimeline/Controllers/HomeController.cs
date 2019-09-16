a3f7d7eb0f9e7fb9df5e52502e1889cc2a52f456
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-09-15 21:38:38 -0700

    More pages in place
    Added admin, an apply page for article submissions, and a search page which lists all approved articles

--- EarthsTimeline/Controllers/HomeController.cs
+++ EarthsTimeline/Controllers/HomeController.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EarthsTimeline.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EarthsTimeline.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly TimelineContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, TimelineContext context)
         {
             _logger = logger;
+            _context = context;
+        }
+
+        [Route("/admin")]
+        public IActionResult Admin()
+        {
+            return View();
         }
 
         public IActionResult Index()
@@ -23,6 +32,34 @@ namespace EarthsTimeline.Controllers
             return View();
         }
 
+        [Route("/search")]
+        public async Task<IActionResult> Search()
+        {
+            return View(await _context.Article.Where((x) => x.Approved == true).ToListAsync());
+        }
+
+        [HttpGet("/apply")]
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost("/apply")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Apply([Bind("Id,AuthorId,AuthorName,LargeThumbnailURL,SmallThumbnailURL,Title,Date,SummaryShort,SummaryLong,Content")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                article.Approved = false;
+                _context.Add(article);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(article);
+        }
+
+        [Route("/privacy")]
         public IActionResult Privacy()
         {
             return View();
