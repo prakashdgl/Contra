6f495abf44378f3aabcd62a1fcbff15c74924f13
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-09-15 18:45:00 -0700

    Entity Framework Core setup
    Added articles and comments models

---
+++ EarthsTimeline/Models/Article.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EarthsTimeline.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public bool Approved { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string LargeThumbnailURL { get; set; }
+        public string SmallThumbnailURL { get; set; }
+
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string SummaryShort { get; set; }
+        public string SummaryLong { get; set; }
+
+        public string Content { get; set; }
+
+        public IEnumerable<Comment> Comments { get; set; }
+    }
+}
