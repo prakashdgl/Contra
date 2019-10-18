6971ee2242f98a21c155a355c96476aa2f99e69e
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-10-17 21:52:41 -0700

    AdminController
    Moved admin panels and login to a separate controller

--- EarthsTimeline/Controllers/HomeController.cs
+++ EarthsTimeline/Controllers/HomeController.cs
@@ -28,52 +28,6 @@ namespace EarthsTimeline.Controllers
                 return false;
         }
 
-        [Route("/logout")]
-        public IActionResult Logout()
-        {
-            Response.Cookies.Delete("AntiForge");
-            return Redirect("~/");
-        }
-
-        [Route("/login")]
-        [HttpGet]
-        public IActionResult Login()
-        {
-            if (LoggedIn()) return Redirect("~/admin");
-            return View();
-        }
-
-        [Route("/login")]
-        [HttpPost]
-        public IActionResult Login(string User)
-        {
-            if (User == "epstein, please no")
-            {
-                Response.Cookies.Append("AntiForge", "UUDDLRLRBABAS", 
-                        new CookieOptions() { Path = "/", Expires = DateTime.Now.AddDays(1), IsEssential = true } );
-                return Redirect("~/admin");
-            }
-            else return View();
-        }
-
-        [Route("/admin")]
-        public async Task<IActionResult> Admin()
-        {
-            if (!LoggedIn()) return Redirect("~/login");
-
-            List<Article> articles = await _context.Article.Where((x) => x.Approved == false).ToListAsync();
-            ViewData["Articles"] = _context.Article.Count();
-            ViewData["ArticlesList"] = articles;
-            ViewData["ArticlesLeft"] = articles.Count;
-
-            List<Comment> comments = await _context.Comment.Where((x) => x.Approved == false).ToListAsync();
-            ViewData["Comments"] = _context.Comment.Count();
-            ViewData["CommentsList"] = comments;
-            ViewData["CommentsLeft"] = comments.Count;
-
-            return View();
-        }
-
         public IActionResult ApproveArticle(int id)
         {
             if (!LoggedIn()) return Redirect("~/");
