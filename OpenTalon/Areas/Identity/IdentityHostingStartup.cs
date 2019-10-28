991fca2225a5322743df760ec5f9fb680c582bb8
Kyle <35588499+SDBagel@users.noreply.github.com>
2019-10-27 21:55:20 -0700

    Add name property to account management/registration
    Added name property to account process
    
    Overriden registration and login pages to make them look better

---
+++ OpenTalon/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTalon.Areas.Identity.Data;
+using OpenTalon.Data;
+
+[assembly: HostingStartup(typeof(OpenTalon.Areas.Identity.IdentityHostingStartup))]
+namespace OpenTalon.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
